f11e5040aa6ade89e03267e9ba894ae62b5f134c
Dawid Salamon <luckyluj24@gmail.com>
2023-04-07 19:51:12 +0200

    Maps maps where gold. closes #4

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -3,7 +3,6 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
 public interface PetService extends CrudService<Pet,Long>{
 
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -3,7 +3,6 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
 public interface VetService  extends CrudService<Vet,Long> {
     Vet findByLastName(String lastName);
