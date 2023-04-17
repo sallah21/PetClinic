9e0d062c031252d69da2b816fa0efc95df90f99c
Dawid Salamon <luckyluj24@gmail.com>
2023-04-17 20:28:14 +0200

    Naprawiłem działa. Closes #2

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgDiApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgDiApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(SfgDiApplication.class, args);
+
 		SpringApplication.run(SfgDiApplication.class, args);
 	}
 
