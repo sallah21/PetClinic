c6d19f56512611367954597684e7dbf657a21586
Dawid Salamon <luckyluj24@gmail.com>
2023-05-16 16:10:21 +0200

    Porper id generation. Closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -18,20 +18,15 @@ public class DataLoader  implements CommandLineRunner {
         this.vetService = vetService;
     }
 
-
-
-
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Adrian");
         owner1.setLastName("Wiechec");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
@@ -41,7 +36,6 @@ public class DataLoader  implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
@@ -49,7 +43,6 @@ public class DataLoader  implements CommandLineRunner {
 
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
