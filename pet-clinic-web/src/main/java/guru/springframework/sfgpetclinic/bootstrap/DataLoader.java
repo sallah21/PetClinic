6b57455fd60f35978b18f944222a6ad204321bc4
Dawid Salamon <luckyluj24@gmail.com>
2023-05-16 14:09:14 +0200

    Dodałem spring config. Closes #22

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -13,10 +13,13 @@ import org.springframework.stereotype.Component;
 public class DataLoader  implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
+
+
+
 
     @Override
     public void run(String... args) throws Exception {
