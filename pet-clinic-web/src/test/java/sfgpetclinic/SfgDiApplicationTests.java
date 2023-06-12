674a70d91964371029ea77780863ef6df7d49fcc
Dawid Salamon <luckyluj24@gmail.com>
2023-06-12 22:31:54 +0200

    Message bug fix and packagking test bug found and fixed. Closes #25

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgDiApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgDiApplicationTests.java
@@ -1,9 +1,13 @@
 package sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.SfgDiApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SfgDiApplication.class)
 class SfgDiApplicationTests {
 
 	@Test
