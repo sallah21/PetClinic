216bda7e0846ad16aad3467b42d4808cfe0b94ab
Dawid Salamon <luckyluj24@gmail.com>
2023-06-13 14:33:02 +0200

    Pet type map service implemented. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
